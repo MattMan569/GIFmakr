1345c587c3bf676ee2bb50e39cce9560694c6752
MattMan569 <matthewpolsom@hotmail.com>
2021-03-30 02:54:41 +0000

    add server

---
+++ server.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var express = require('express');
+var morgan = require('morgan');
+
+var app = express();
+
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+    res.set({
+            'Cross-Origin-Opener-Policy': 'same-origin',
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+        });
+    next();
+})
+app.use(express.static(__dirname + '/dist/GIFmakr'));
+
+app.listen(process.env.PORT, () => {
+    console.log('GIFmakr is up on port ', process.env.PORT);
+});
